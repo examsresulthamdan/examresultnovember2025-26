5a40542a9ed7014cb0f28d4ac6055d91bb584493
examsresulthamdan <examsresulthamdan@gmail.com>
2025-11-16 05:55:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,286 @@
+const classes = [
+
+  { key: "class1", name: "01 - Tamhīdiyya Ūla" },
+
+  { key: "class2", name: "02 - Tamhīdiyya Thāniya" },
+
+  { key: "class3", name: "03 - Āliya Ūla" },
+
+  { key: "class4", name: "04 - Āliya Thāniya" },
+
+  { key: "class5", name: "05 - Āliya Thālitha" },
+
+  { key: "class6", name: "06 - Mājastīr Awwal" },
+
+  { key: "class7", name: "07 - Mājastīr Thāni" }
+
+];
+
+
+const data = {
+  class1: {
+    students: {
+      "Muhammed Yaseen KV": { "password": "19062009", "image": "results/class1/muhammed_yaseen_kv.jpg" },
+    "Hameed Munavvar": { "password": "04012010", "image": "results/class1/hameed_munavvar.jpg" },
+    "Muhammed Yaseen VN": { "password": "13072009", "image": "results/class1/muhammed_yaseen_vn.jpg" },
+    "Muhammed Swabeeh": { "password": "03102009", "image": "results/class1/muhammed_swabeeh.jpg" },
+    "Muhammed Rashik S": { "password": "25112009", "image": "results/class1/muhammed_rashik_s.jpg" },
+    "Muhammed Sinan TM": { "password": "12042009", "image": "results/class1/muhammed_sinan_tm.jpg" },
+    "Abrar Ahammed YS": { "password": "15042009", "image": "results/class1/abrar_ahammed_ys.jpg" },
+    "Abdul Basith H": { "password": "12012009", "image": "results/class1/abdul_basith_h.jpg" },
+    "Muhammed Koya Thangal N": { "password": "04052009", "image": "results/class1/muhammed_koya_thangal_n.jpg" },
+    "Muhammed Rafil K": { "password": "08042008", "image": "results/class1/muhammed_rafil_k.jpg" },
+    "Muhammed Dhanish KC": { "password": "10072009", "image": "results/class1/muhammed_dhanish_kc.jpg" },
+    "Aalim Siyan S": { "password": "09062009", "image": "results/class1/aalim_siyan_s.jpg" },
+    "A Muhammed Rashid": { "password": "02072009", "image": "results/class1/a_muhammed_rashid.jpg" },
+    "Muhammed Adhil N": { "password": "19082009", "image": "results/class1/muhammed_adhil_n.jpg" },
+    "Anas A": { "password": "22102010", "image": "results/class1/anas_a.jpg" },
+    "Muhammad Yusuf": { "password": "26042009", "image": "results/class1/muhammad_yusuf.jpg" },
+    "Muhammad Bilal": { "password": "30112009", "image": "results/class1/muhammad_bilal.jpg" },
+    "Shibin S": { "password": "05042008", "image": "results/class1/shibin_s.jpg" },
+    "Muhammad Yaseen R": { "password": "18032009", "image": "results/class1/muhammad_yaseen_r.jpg" },
+    "Muhammed Aslam": { "password": "30122009", "image": "results/class1/muhammed_aslam.jpg" },
+    "Alsabith M": { "password": "29042008", "image": "results/class1/alsabith_m.jpg" },
+    "Muhammad Mehfin": { "password": "06032008", "image": "results/class1/muhammad_mehfin.jpg" },
+    "Muhammed Nabeel A": { "password": "02072009", "image": "results/class1/muhammed_nabeel_a.jpg" },
+    "Ahmad Raihan": { "password": "23092007", "image": "results/class1/ahmad_raihan.jpg" },
+    "Sinan Ahmed": { "password": "13122008", "image": "results/class1/sinan_ahmed.jpg" }
+    }
+  },
+
+  // Keep space for other classes to add later:
+class2: {
+  students: {
+    "Muhammed Ramees K": { password: "29062007", image: "results/class2/muhammed_ramees_k.jpg" },
+    "Muhammed Sufiyan KK": { password: "31072008", image: "results/class2/muhammed_sufiyan_kk.jpg" },
+    "Anshab MK": { password: "19082008", image: "results/class2/anshab_mk.jpg" },
+    "Mohammed Nashid K": { password: "12122007", image: "results/class2/mohammed_nashid_k.jpg" },
+    "Muhammad Salman S": { password: "07092008", image: "results/class2/muhammad_salman_s.jpg" },
+    "Adil N": { password: "26032007", image: "results/class2/adil_n.jpg" },
+    "Muhammed Ansar PU": { password: "04062009", image: "results/class2/muhammed_ansar_pu.jpg" },
+    "Muhammed Zayan O": { password: "07082008", image: "results/class2/muhammed_zayan_o.jpg" },
+    "Muhammed Najah V": { password: "22012008", image: "results/class2/muhammed_najah_v.jpg" },
+    "Abdul Ahad": { password: "25062008", image: "results/class2/abdul_ahad.jpg" },
+    "Muhammed Ihsan NH": { password: "07032008", image: "results/class2/muhammed_ihsan_nh.jpg" },
+    "Muhammed Sahil SS": { password: "01122008", image: "results/class2/muhammed_sahil_ss.jpg" },
+    "Muhammed Umar Mukhtar A": { password: "08012008", image: "results/class2/muhammed_umar_mukhtar_a.jpg" },
+    "Muhammed Finan C": { password: "15112006", image: "results/class2/muhammed_finan_c.jpg" },
+    "Abdul Rahuman R": { password: "05012007", image: "results/class2/abdul_rahuman_r.jpg" },
+    "Ahammed Faaiz": { password: "16072007", image: "results/class2/ahammed_faaiz.jpg" },
+    "Abrarul Haq": { password: "17122007", image: "results/class2/abrarul_haq.jpg" },
+    "Muhammed Irfan I": { password: "10082008", image: "results/class2/muhammed_irfan_i.jpg" },
+    "Anshid Ali M": { password: "08062006", image: "results/class2/anshid_ali_m.jpg" },
+    "Arif Muhammed": { password: "02092008", image: "results/class2/arif_muhammed.jpg" },
+    "Muhammed Safwan": { password: "04062009", image: "results/class2/muhammed_safwan.jpg" },
+    "Muhammed Aadil S": { password: "18022008", image: "results/class2/muhammed_aadil_s.jpg" },
+    "Abdul Nafih T": { password: "20092008", image: "results/class2/abdul_nafih_t.jpg" },
+    "Muhammed Asjad": { password: "05012007", image: "results/class2/muhammed_asjad.jpg" },
+    "Faaiz Muhammed A": { password: "21092007", image: "results/class2/faaiz_muhammed_a.jpg" },
+    "Ahmed Kabeer KA": { password: "08012006", image: "results/class2/ahmed_kabeer_ka.jpg" },
+    "Fahrudheen TF": { password: "01062009", image: "results/class2/fahrudheen_tf.jpg" },
+    "Anas Iyas": { password: "18112007", image: "results/class2/anas_iyas.jpg" },
+    "Arif Khan S": { password: "17062008", image: "results/class2/arif_khan_s.jpg" },
+    "Muhammed Mishab A": { password: "03022008", image: "results/class2/muhammed_mishab_a.jpg" },
+    "Muhammed Shehin": { password: "01102008", image: "results/class2/muhammed_shehin.jpg" },
+    "Ashfaqullah Khan": { password: "07092007", image: "results/class2/ashfaqullah_khan.jpg" }
+  }
+},
+
+
+    class3: {
+    students: {
+      "Muhammed Ashmil NP": { password: "27062006", image: "results/class3/muhammed_ashmil_np.jpg" },
+      "Ahammed Irfan AR": { password: "16112006", image: "results/class3/ahammed_irfan_ar.jpg" },
+      "Muhammed Arafath": { password: "16032007", image: "results/class3/muhammed_arafath.jpg" },
+      "Mohamed Junaid M": { password: "07102006", image: "results/class3/mohamed_junaid_m.jpg" },
+      "Muhammed Nisil M": { password: "05052007", image: "results/class3/muhammed_nisil_m.jpg" },
+      "Abdul Basith A": { password: "20122007", image: "results/class3/abdul_basith_a.jpg" },
+      "Hamdan Sha M": { password: "24042007", image: "results/class3/hamdan_sha_m.jpg" },
+      "Muhammad Misbah LA": { password: "10102006", image: "results/class3/muhammad_misbah_la.jpg" },
+      "Abdulla Irfan LA": { password: "10082005", image: "results/class3/abdulla_irfan_la.jpg" },
+      "Aman Muhammed AS": { password: "15022007", image: "results/class3/aman_muhammed_as.jpg" },
+      "Muhammed Selman N": { password: "15062007", image: "results/class3/muhammed_selman_n.jpg" },
+      "Minhaj Fadel M": { password: "10112006", image: "results/class3/minhaj_fadel_m.jpg" },
+      "Muhammed Shuhaib R": { password: "19122006", image: "results/class3/muhammed_shuhaib_r.jpg" },
+      "Muhammad Rizwan A": { password: "15112006", image: "results/class3/muhammad_rizwan_a.jpg" },
+      "Muhammed Ihsan P": { password: "10122006", image: "results/class3/muhammed_ihsan_p.jpg" },
+      "Muhammed Shahin S": { password: "23082006", image: "results/class3/muhammed_shahin_s.jpg" },
+      "Muhammed Althaf JB": { password: "16082006", image: "results/class3/muhammed_althaf_jb.jpg" },
+      "Muhammed Sufyaan A": { password: "15052007", image: "results/class3/muhammed_sufyaan_a.jpg" },
+      "Muhammed Farooq N": { password: "09062006", image: "results/class3/muhammed_farooq_n.jpg" },
+      "Afsal Muhammad A": { password: "21012007", image: "results/class3/afsal_muhammad_a.jpg" },
+      "Mazin Ahmed": { password: "08072006", image: "results/class3/mazin_ahmed.jpg" },
+      "Nibras Ali K": { password: "23072005", image: "results/class3/nibras_ali_k.jpg" }
+    }
+  },
+  class4: {
+  students: {
+    "Muhammed Sinan P": { password: "10112006", image: "results/class4/muhammed_sinan_p.jpg" },
+    "Javed Iqbal": { password: "04022006", image: "results/class4/javed_iqbal.jpg" },
+    "Murshid Muhammed I": { password: "13072006", image: "results/class4/murshid_muhammed_i.jpg" },
+    "Amanudeen S": { password: "24092005", image: "results/class4/amanudeen_s.jpg" },
+    "Muhammed Ashkar H": { password: "28042006", image: "results/class4/muhammed_ashkar_h.jpg" },
+    "Febin Robinson": { password: "08042006", image: "results/class4/febin_robinson.jpg" },
+    "Muhammed Muhsin A": { password: "11082005", image: "results/class4/muhammed_muhsin_a.jpg" },
+    "Fahad Shereef": { password: "19082005", image: "results/class4/fahad_shereef.jpg" },
+    "Rayan Muhammed Basheer": { password: "30082004", image: "results/class4/rayan_muhammed_basheer.jpg" },
+    "Muhammed Jasir AK": { password: "11022004", image: "results/class4/muhammed_jasir_ak.jpg" },
+    "Sahid Muhzin": { password: "06122004", image: "results/class4/sahid_muhzin.jpg" },
+    "Abdullah N": { password: "19092005", image: "results/class4/abdullah_n.jpg" },
+    "Muhammed Thoufeeq": { password: "24122005", image: "results/class4/muhammed_thoufeeq.jpg" },
+    "Azharudheen PJ": { password: "25062005", image: "results/class4/azharudheen_pj.jpg" }
+  }
+},
+
+  class5: {
+  students: {
+    "Suhail A": { 
+      password: "13012005", 
+      image: "results/class5/suhail_a.jpg" 
+    },
+    "Muhammed Nabeel SS": { 
+      password: "10012005", 
+      image: "results/class5/muhammed_nabeel_ss.jpg" 
+    },
+    "Mohammed Anas Rahim": { 
+      password: "10082005", 
+      image: "results/class5/mohammed_anas_rahim.jpg" 
+    },
+    "Muhammed Al Ameen": { 
+      password: "23112005", 
+      image: "results/class5/muhammed_al_ameen.jpg" 
+    },
+    "Abdul Basith": { 
+      password: "18042005", 
+      image: "results/class5/abdul_basith.jpg" 
+    },
+    "Muhammed Ihsan": { 
+      password: "22012005", 
+      image: "results/class5/muhammed_ihsan.jpg" 
+    },
+    "Muhammed Aamir AR": { 
+      password: "13122005", 
+      image: "results/class5/muhammed_aamir_ar.jpg" 
+    }
+  }
+},
+
+  class6: {
+  students: {
+    "Muhammed Rashid J": { password: "04102003", image: "results/class6/muhammed_rashid_j.jpg" },
+    "Hashir H": { password: "17032005", image: "results/class6/hashir_h.jpg" },
+    "Muhammed Saif M": { password: "01072003", image: "results/class6/muhammed_saif_m.jpg" },
+    "Muhammed Shanif KV": { password: "02072004", image: "results/class6/muhammed_shanif_kv.jpg" },
+    "Muhammed Sahil S": { password: "23072003", image: "results/class6/muhammed_sahil_s.jpg" },
+    "Muhammed Irfan Yoonas": { password: "06092004", image: "results/class6/muhammed_irfan_yoonas.jpg" },
+    "Saneen Muhammed A": { password: "28092004", image: "results/class6/saneen_muhammed_a.jpg" },
+    "Misshab Rahuman R": { password: "07042003", image: "results/class6/misshab_rahuman_r.jpg" },
+    "Muhammed Shuhaib S": { password: "30052004", image: "results/class6/muhammed_shuhaib_s.jpg" },
+    "Muhammed Asker U": { password: "02052003", image: "results/class6/muhammed_asker_u.jpg" },
+    "Anvar Ayoob": { password: "14042004", image: "results/class6/anvar_ayoob.jpg" },
+    "Muhammed Suhail CS": { password: "31122004", image: "results/class6/muhammed_suhail_cs.jpg" },
+    "Shahinshad T": { password: "24062002", image: "results/class6/shahinshad_t.jpg" },
+    "Muhammed Nabhan K": { password: "18112003", image: "results/class6/muhammed_nabhan_k.jpg" },
+    "Abdul Basith": { password: "16062003", image: "results/class6/abdul_basith.jpg" },
+    "Bilal MV": { password: "30082003", image: "results/class6/bilal_mv.jpg" },
+    "Muhammed Unais NU": { password: "14022004", image: "results/class6/muhammed_unais_nu.jpg" },
+    "Afthab Rahman VP": { password: "05032004", image: "results/class6/afthab_rahman_vp.jpg" }
+  }
+},
+
+  class7: {
+  students: {
+    "Muhammed Aslam H": { password: "03032002", image: "results/class7/muhammed_aslam_h.jpg" },
+    "Muhammed Althaf J": { password: "16042003", image: "results/class7/muhammed_althaf_j.jpg" },
+    "A Abrar Ahmad": { password: "23052002", image: "results/class7/a_abrar_ahmad.jpg" },
+    "Muhammed Adhil S": { password: "30062002", image: "results/class7/muhammed_adhil_s.jpg" },
+    "Ajmal Rasheed": { password: "04062002", image: "results/class7/ajmal_rasheed.jpg" },
+    "Muhammed Shibli KP": { password: "21012003", image: "results/class7/muhammed_shibli_kp.jpg" },
+    "Salih S": { password: "04012003", image: "results/class7/salih_s.jpg" },
+    "Afzal N": { password: "01012003", image: "results/class7/afzal_n.jpg" },
+    "Muhammad Fayiz HR": { password: "22022001", image: "results/class7/muhammad_fayiz_hr.jpg" },
+    "Rafeeq": { password: "28112002", image: "results/class7/rafeeq.jpg" },
+    "Muhammed Arshad NN": { password: "24042003", image: "results/class7/muhammed_arshad_nn.jpg" },
+    "Razeen Ahmad": { password: "05042002", image: "results/class7/razeen_ahmad.jpg" },
+    "Anwarsha Badarudeen": { password: "17062002", image: "results/class7/anwarsha_badarudeen.jpg" },
+    "Muhammed Ameen RT": { password: "09122003", image: "results/class7/muhammed_ameen_rt.jpg" },
+    "Shamil P": { password: "16012002", image: "results/class7/shamil_p.jpg" },
+    "Nihal EP": { password: "24092001", image: "results/class7/nihal_ep.jpg" },
+    "Noufal T": { password: "29102001", image: "results/class7/noufal_t.jpg" },
+    "Roshan Ahmed": { password: "27072002", image: "results/class7/roshan_ahmed.jpg" },
+    "Mohammed Suhayl EA": { password: "20022003", image: "results/class7/mohammed_suhayl_ea.jpg" }
+  }
+}
+
+};
+
+
+let selectedClassKey = null;
+let selectedStudentName = null;
+
+function sanitizeForFilename(name) {
+  return name.trim().toLowerCase().replace(/[^a-z0-9Ā-ſ]+/g, "_").replace(/^_+|_+$/g, "");
+}
+
+function renderClassButtons() {
+  const container = document.getElementById('classContainer');
+  container.innerHTML = '';
+  classes.forEach(c => {
+    const btn = document.createElement('button');
+    btn.className = 'class-btn';
+    btn.innerText = c.name;
+    btn.onclick = () => openClass(c.key);
+    container.appendChild(btn);
+  });
+}
+
+function openClass(classKey) {
+  selectedClassKey = classKey;
+  document.getElementById('classTitle').innerText = classes.find(c => c.key === classKey).name;
+  const studentList = document.getElementById('studentList');
+  studentList.innerHTML = '';
+  const students = data[classKey].students || { };
+  const names = Object.keys(students).sort((a,b) => a.localeCompare(b));
+  if (names.length === 0) {
+    const p = document.createElement('p');
+    p.innerText = "No students found for this class.";
+    studentList.appendChild(p);
+  } else {
+    names.forEach(student => {
+      const btn = document.createElement('button');
+      btn.innerText = student;
+      btn.onclick = () => openPasswordPopup(student);
+      studentList.appendChild(btn);
+    });
+  }
+  document.getElementById('studentPopup').style.display = 'flex';
+}
+
+function openPasswordPopup(studentName) {
+  selectedStudentName = studentName;
+  document.getElementById('studentNameTitle').innerText = studentName;
+  document.getElementById('passwordInput').value = '';
+  document.getElementById('passwordPopup').style.display = 'flex';
+}
+
+function verifyPassword() {
+  const entered = document.getElementById('passwordInput').value.trim();
+  const student = data[selectedClassKey].students[selectedStudentName];
+  if (!student) { alert("Student data not found."); return; }
+  const correct = String(student.password || "");
+  if (entered === correct) {
+    let imgPath = student.image;
+    if (!imgPath) imgPath = 'results/' + selectedClassKey + '/' + sanitizeForFilename(selectedStudentName) + '.jpg';
+    const imgEl = document.getElementById('resultImage');
+    imgEl.src = imgPath;
+    imgEl.onerror = function() { this.src = ''; alert('Result image not found at: ' + imgPath + '\n\nPlease check that the image file exists and path is correct.'); };
+    document.getElementById('resultPopup').style.display = 'flex';
+    closePopup('passwordPopup');
+  } else { alert('Incorrect Password!'); }
+}
+
+function closePopup(id) { document.getElementById(id).style.display = 'none'; }
+
+function downloadImage() { const img = document.getElementById('resultImage'); if (!img || !img.src) { alert('No image to download'); return; } const link = document.createElement('a'); link.href = img.src; const filename = sanitizeForFilename(selectedStudentName || 'result') + '_result.jpg'; link.download = filename; link.click(); }
+
+renderClassButtons();
